0af5914cba52df82b3714ccc40fdded56a90beea
willynov <william.galby@ynov.com>
2018-01-26 16:21:46 +0100

    Exercise 4

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express();
-
+var fs = require('fs'); 
 app.get('/bingo', function(req, res) {
-    res.end('Bingo')
+    fs.readFile(__dirname + '/numbers.txt', function (err, data) {
+        //il faut que le fichier ne termine pas par un retour chariot sinon data mettra une virgule à la fin
+        res.end(data.toString().split(/\r/).join(',').replace(/\n|\r/g,''));
+    });
 })
 
 module.exports = app;
